fc0f02469a541d8a173d821fd640c9adc792595d
funtushan@gmail.com <funtusthan@gmail.com>
2020-02-16 14:20:58 +0200

    add test

--- src/test/java/ua/com/alevel/security/SecurityApplicationTests.java
+++ src/test/java/ua/com/alevel/security/SecurityApplicationTests.java
@@ -2,12 +2,23 @@ package ua.com.alevel.security;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootTest
 class SecurityApplicationTests {
 
 	@Test
 	void contextLoads() {
+		PasswordEncoder encoder = new BCryptPasswordEncoder();
+		String pass = "admin";
+		System.out.println("encoder = " + encoder.encode(pass));
+		System.out.println("encoder = " + encoder.encode(pass));
+		System.out.println("encoder = " + encoder.encode(pass));
+
+		String passEncoder = encoder.encode(pass);
+
+		System.out.println("encoder = " + encoder.matches(pass, passEncoder));
 	}
 
 }
